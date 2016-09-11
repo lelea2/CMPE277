a01b3a101f37eda2c61e96a3c2bb50aa980520e3
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-11 13:09:34 -0700

    Initial check in

--- Assignment1/app/src/main/java/com/example/kdao/assignment1/ActivityA.java
+++ Assignment1/app/src/main/java/com/example/kdao/assignment1/ActivityA.java
@@ -17,21 +17,26 @@ import android.widget.TextView;
 
 public class ActivityA extends AppCompatActivity {
 
+    private TextView threadCount;
+    private TextView bundleCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        threadCount = (TextView)findViewById(R.id.threads_count);
+        bundleCount = (TextView)findViewById(R.id.bundles_count);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
