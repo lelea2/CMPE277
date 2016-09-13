7906c92f3bc46afaaa106e8f2d597f207530404e
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-12 23:46:01 -0700

    Initial check in for assignment2

--- Assignment1/app/src/main/java/com/example/kdao/assignment1/ActivityA.java
+++ Assignment1/app/src/main/java/com/example/kdao/assignment1/ActivityA.java
@@ -41,6 +41,9 @@ public class ActivityA extends AppCompatActivity {
 
     @Override
     protected void onResume() {
+        System.out.println(">>> on resume <<<");
+        thread_numb = thread_numb + 1;
+        threadCount.setText(String.format("%05d", thread_numb));
         super.onResume();
     }
 
