93fc7a23265483eb5cbf1371d17b3be6bc6f3b3d
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-16 08:56:30 -0700

    Initial check in

---
+++ IoT_Farm_BroadcastReceiver/app/src/main/java/com/example/kdao/iot_farm_broadcastreceiver/FarmMaintenance.java
@@ -0,0 +1,13 @@
+package com.example.kdao.iot_farm_broadcastreceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmMaintenance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_maintenance);
+    }
+}
