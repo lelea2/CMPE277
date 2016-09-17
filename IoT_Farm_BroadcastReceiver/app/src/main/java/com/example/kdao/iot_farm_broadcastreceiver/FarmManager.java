0892d0e8d2684abf6fda9dadde3fc204bf4e6ba2
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-17 12:29:51 -0700

    Initial check in for for android layout

--- IoT_Farm_BroadcastReceiver/app/src/main/java/com/example/kdao/iot_farm_broadcastreceiver/FarmManager.java
+++ IoT_Farm_BroadcastReceiver/app/src/main/java/com/example/kdao/iot_farm_broadcastreceiver/FarmManager.java
@@ -2,6 +2,8 @@ package com.example.kdao.iot_farm_broadcastreceiver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class FarmManager extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class FarmManager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_manager);
+    }
+
+    public void turnFanOn(View v) {
+
+    }
+
+    public void turnFanSprinklerOn(View v) {
+
+    }
+
+    public void backToSettings(View v) {
+        Intent intent = new Intent(FarmManager.this, MainActivity.class);
+        startActivity(intent);
     }
 }
