81a3da5d131e8f4f6ab0c0b804be16355fd51543
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-18 04:00:48 -0700

    Adding handler broadcast receiver

--- IoT_Farm_BroadcastReceiver/app/src/main/java/com/example/kdao/iot_farm_broadcastreceiver/FarmMaintenance.java
+++ IoT_Farm_BroadcastReceiver/app/src/main/java/com/example/kdao/iot_farm_broadcastreceiver/FarmMaintenance.java
@@ -1,7 +1,12 @@
 package com.example.kdao.iot_farm_broadcastreceiver;
 
+import android.content.BroadcastReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.Context;
+import android.widget.Toast;
+import android.os.Handler;
 
 public class FarmMaintenance extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class FarmMaintenance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_maintenance);
+    }
+
+    public static class MyReceiver extends BroadcastReceiver {
+
+        private final Handler handler; // Handler used to execute code on the UI thread
+
+        public MyReceiver(Handler handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            // Post the UI updating code to our Handler
+            handler.post(new Runnable() {
+                @Override
+                    public void run() {
+                    Toast.makeText(context, "Toast from broadcast receiver", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
