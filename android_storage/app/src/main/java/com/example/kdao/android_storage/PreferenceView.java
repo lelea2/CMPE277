1bc9d5f184f8cd4e3e77b8c1a2e4b27524bdfe6d
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-20 21:08:21 -0700

    Initial check in for app storage layout

---
+++ android_storage/app/src/main/java/com/example/kdao/android_storage/PreferenceView.java
@@ -0,0 +1,13 @@
+package com.example.kdao.android_storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreferenceView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference_view);
+    }
+}
