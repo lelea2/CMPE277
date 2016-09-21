bbc065d603fb7481423f1042c6caa0385f0227ee
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-20 21:28:41 -0700

    Initial check in

--- android_storage/app/src/main/java/com/example/kdao/android_storage/PreferenceView.java
+++ android_storage/app/src/main/java/com/example/kdao/android_storage/PreferenceView.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class PreferenceView extends AppCompatActivity {
+
+    private EditText bookName;
+    private EditText authorName;
+    private EditText desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference_view);
+        bookName = (EditText) findViewById(R.id.book_name_text);
+        authorName = (EditText) findViewById(R.id.book_name_text);
+        desc = (EditText) findViewById(R.id.desc_text);
     }
 
     public void savePreference(View v) {
--- android_storage/app/src/main/java/com/example/kdao/android_storage/SQLiteView.java
+++ android_storage/app/src/main/java/com/example/kdao/android_storage/SQLiteView.java
@@ -4,19 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SQLiteView extends AppCompatActivity {
+
+    private EditText desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqlite_view);
+        desc = (EditText) findViewById(R.id.desc_text);
     }
 
     public void saveSQLite(View v) {
 
     }
-    
+
     public void cancelActivity(View v) {
         Intent intent = new Intent(SQLiteView.this, MainActivity.class);
         startActivity(intent);
