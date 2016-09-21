d1f697e16b3fc1f6593327c5b8b5e21aaed08243
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-21 00:35:15 -0700

    Initial check in for broadcast app

---
+++ Assignment3_IOT_Farm/FarmManager/app/src/main/java/com/example/kdao/iot_broadcastreceiver_setter/FarmManager.java
@@ -0,0 +1,22 @@
+package com.example.kdao.iot_broadcastreceiver_setter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FarmManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_manager);
+    }
+
+    public void turnFanOn(View v) {
+
+    }
+
+    public void turnFanSprinklerOn(View v) {
+
+    }
+}
