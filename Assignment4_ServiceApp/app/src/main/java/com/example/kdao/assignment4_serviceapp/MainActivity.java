1cb0caba4655e3cd5ed24d034baecbbacc0163f5
Dao <kdao@kdao01sjl.attlocal.net>
2016-09-22 21:27:25 -0700

    Initial check in for services app

--- Assignment4_ServiceApp/app/src/main/java/com/example/kdao/assignment4_serviceapp/MainActivity.java
+++ Assignment4_ServiceApp/app/src/main/java/com/example/kdao/assignment4_serviceapp/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.kdao.assignment4_serviceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText file1;
+    private EditText file2;
+    private EditText file3;
+    private EditText file4;
+    private EditText file5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        file1 = (EditText) findViewById(R.id.file_1);
+        file2 = (EditText) findViewById(R.id.file_2);
+        file3 = (EditText) findViewById(R.id.file_3);
+        file4 = (EditText) findViewById(R.id.file_4);
+        file5 = (EditText) findViewById(R.id.file_5);
+    }
+
+    public void downloadFile(View v) {
+
     }
 }
