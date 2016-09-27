f9a23282e6e8e9a7d368e3ef8f031dd05a3697e8
lelea2 <daomaikhanh1988@yahoo.com>
2016-09-26 21:25:45 -0700

    Initial check in for dowload

--- Assignment4_ServiceApp/app/src/main/java/com/example/kdao/assignment4_serviceapp/MainActivity.java
+++ Assignment4_ServiceApp/app/src/main/java/com/example/kdao/assignment4_serviceapp/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.content.Intent;
+import java.net.URL;
+import java.net.MalformedURLException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void downloadFile(View v) {
-
+        Intent intent = new Intent(getBaseContext(), MyService.class);
+        String url1 = file1.getText().toString();
+        String url2 = file2.getText().toString();
+        String url3 = file3.getText().toString();
+        String url4 = file4.getText().toString();
+        String url5 = file5.getText().toString();
+        try {
+            URL[] urls = new URL[]{
+                    new URL(url1),
+                    new URL(url2),
+                    new URL(url3),
+                    new URL(url4),
+                    new URL(url5)
+                };
+            intent.putExtra("URLs", urls);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        startService(intent);
     }
+
+    private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(getBaseContext(), "File downloaded!", Toast.LENGTH_LONG).show();
+        }
+    };
+
 }
